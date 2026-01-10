359b740f65feec5a50f55241825c54403b623aaf
RedPaladin7 <73498670+RedPaladin7@users.noreply.github.com>
2026-01-10 18:10:24 +0530

    Update events.go

--- internal/blockchain/events.go
+++ internal/blockchain/events.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"strings"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,59 +18,69 @@ import (
 type EventListener struct {
 	bc          *BlockchainClient
 	subscribers map[string][]chan interface{}
+	contractABI abi.ABI
 }
 
 // NewEventListener creates a new event listener
 func NewEventListener(bc *BlockchainClient) *EventListener {
+	// Parse contract ABI
+	// In production, this would be loaded from the compiled contract
+	abiJSON := getPokerTableABI()
+	contractABI, err := abi.JSON(strings.NewReader(abiJSON))
+	if err != nil {
+		logrus.Errorf("Failed to parse contract ABI: %v", err)
+	}
+
 	return &EventListener{
 		bc:          bc,
 		subscribers: make(map[string][]chan interface{}),
+		contractABI: contractABI,
 	}
 }
 
 // GameCreatedEvent represents a GameCreated event
 type GameCreatedEvent struct {
-	GameID     [32]byte
-	Creator    common.Address
-	BuyIn      *big.Int
-	MaxPlayers *big.Int
+	GameID      [32]byte
+	Creator     common.Address
+	BuyIn       *big.Int
+	MaxPlayers  *big.Int
 	BlockNumber uint64
-	TxHash     common.Hash
+	TxHash      common.Hash
 }
 
 // PlayerJoinedEvent represents a PlayerJoined event
 type PlayerJoinedEvent struct {
-	GameID  [32]byte
-	Player  common.Address
-	Amount  *big.Int
+	GameID      [32]byte
+	Player      common.Address
+	Amount      *big.Int
 	BlockNumber uint64
-	TxHash  common.Hash
+	TxHash      common.Hash
 }
 
 // GameStartedEvent represents a GameStarted event
 type GameStartedEvent struct {
-	GameID   [32]byte
-	TotalPot *big.Int
+	GameID      [32]byte
+	TotalPot    *big.Int
 	BlockNumber uint64
-	TxHash   common.Hash
+	TxHash      common.Hash
 }
 
 // GameEndedEvent represents a GameEnded event
 type GameEndedEvent struct {
-	GameID   [32]byte
-	Winners  []common.Address
-	Payouts  []*big.Int
+	GameID      [32]byte
+	Winners     []common.Address
+	Payouts     []*big.Int
 	BlockNumber uint64
-	TxHash   common.Hash
+	TxHash      common.Hash
 }
 
 // FundsLockedEvent represents a FundsLocked event
 type FundsLockedEvent struct {
-	GameID  [32]byte
-	Player  common.Address
-	Amount  *big.Int
+	GameID      [32]byte
+	Player      common.Address
+	Amount      *big.Int
 	BlockNumber uint64
-	TxHash  common.Hash
+	TxHash      common.Hash
 }
 
 // Subscribe subscribes to a specific event type
@@ -141,48 +154,229 @@ func (el *EventListener) ListenForEvents(ctx context.Context) error {
 
 // handleLog processes a single log entry
 func (el *EventListener) handleLog(vLog types.Log) {
-	// Parse event based on topic
-	// Topic[0] is the event signature hash
-	
-	// TODO: Implement actual event parsing using contract ABI
-	// This would typically use the generated contract bindings
-	
-	logrus.WithFields(logrus.Fields{
-		"block":   vLog.BlockNumber,
-		"tx_hash": vLog.TxHash.Hex(),
-		"topics":  len(vLog.Topics),
-	}).Debug("Received blockchain event")
+	if len(vLog.Topics) == 0 {
+		return
+	}
 
-	// Example event parsing (would be replaced with actual ABI parsing):
-	// if len(vLog.Topics) > 0 {
-	//     eventSig := vLog.Topics[0].Hex()
-	//     
-	//     switch eventSig {
-	//     case crypto.Keccak256Hash([]byte("GameCreated(bytes32,address,uint256,uint256)")).Hex():
-	//         event := parseGameCreatedEvent(vLog)
-	//         el.publish("GameCreated", event)
-	//     case crypto.Keccak256Hash([]byte("PlayerJoined(bytes32,address,uint256)")).Hex():
-	//         event := parsePlayerJoinedEvent(vLog)
-	//         el.publish("PlayerJoined", event)
-	//     // ... handle other events
-	//     }
-	// }
+	eventSig := vLog.Topics[0]
+
+	logrus.WithFields(logrus.Fields{
+		"block":     vLog.BlockNumber,
+		"tx_hash":   vLog.TxHash.Hex(),
+		"event_sig": eventSig.Hex(),
+	}).Debug("Processing blockchain event")
+
+	// Event signatures
+	gameCreatedSig := crypto.Keccak256Hash([]byte("GameCreated(bytes32,address,uint256,uint256)"))
+	playerJoinedSig := crypto.Keccak256Hash([]byte("PlayerJoined(bytes32,address,uint256)"))
+	gameStartedSig := crypto.Keccak256Hash([]byte("GameStarted(bytes32,uint256)"))
+	gameEndedSig := crypto.Keccak256Hash([]byte("GameEnded(bytes32,address[],uint256[])"))
+	fundsLockedSig := crypto.Keccak256Hash([]byte("FundsLocked(bytes32,address,uint256)"))
+
+	switch eventSig {
+	case gameCreatedSig:
+		event := el.parseGameCreatedEvent(vLog)
+		if event != nil {
+			el.publish("GameCreated", event)
+		}
+
+	case playerJoinedSig:
+		event := el.parsePlayerJoinedEvent(vLog)
+		if event != nil {
+			el.publish("PlayerJoined", event)
+		}
+
+	case gameStartedSig:
+		event := el.parseGameStartedEvent(vLog)
+		if event != nil {
+			el.publish("GameStarted", event)
+		}
+
+	case gameEndedSig:
+		event := el.parseGameEndedEvent(vLog)
+		if event != nil {
+			el.publish("GameEnded", event)
+		}
+
+	case fundsLockedSig:
+		event := el.parseFundsLockedEvent(vLog)
+		if event != nil {
+			el.publish("FundsLocked", event)
+		}
+
+	default:
+		logrus.Debugf("Unknown event signature: %s", eventSig.Hex())
+	}
+}
+
+// parseGameCreatedEvent parses a GameCreated event
+func (el *EventListener) parseGameCreatedEvent(vLog types.Log) *GameCreatedEvent {
+	if len(vLog.Topics) < 2 {
+		return nil
+	}
+
+	var gameID [32]byte
+	copy(gameID[:], vLog.Topics[1].Bytes())
+
+	var creator common.Address
+	copy(creator[:], vLog.Topics[2].Bytes()[12:])
+
+	// Parse data (buyIn, maxPlayers)
+	if len(vLog.Data) < 64 {
+		return nil
+	}
+
+	buyIn := new(big.Int).SetBytes(vLog.Data[0:32])
+	maxPlayers := new(big.Int).SetBytes(vLog.Data[32:64])
+
+	return &GameCreatedEvent{
+		GameID:      gameID,
+		Creator:     creator,
+		BuyIn:       buyIn,
+		MaxPlayers:  maxPlayers,
+		BlockNumber: vLog.BlockNumber,
+		TxHash:      vLog.TxHash,
+	}
+}
+
+// parsePlayerJoinedEvent parses a PlayerJoined event
+func (el *EventListener) parsePlayerJoinedEvent(vLog types.Log) *PlayerJoinedEvent {
+	if len(vLog.Topics) < 2 {
+		return nil
+	}
+
+	var gameID [32]byte
+	copy(gameID[:], vLog.Topics[1].Bytes())
+
+	var player common.Address
+	copy(player[:], vLog.Topics[2].Bytes()[12:])
+
+	if len(vLog.Data) < 32 {
+		return nil
+	}
+
+	amount := new(big.Int).SetBytes(vLog.Data[0:32])
+
+	return &PlayerJoinedEvent{
+		GameID:      gameID,
+		Player:      player,
+		Amount:      amount,
+		BlockNumber: vLog.BlockNumber,
+		TxHash:      vLog.TxHash,
+	}
+}
+
+// parseGameStartedEvent parses a GameStarted event
+func (el *EventListener) parseGameStartedEvent(vLog types.Log) *GameStartedEvent {
+	if len(vLog.Topics) < 2 {
+		return nil
+	}
+
+	var gameID [32]byte
+	copy(gameID[:], vLog.Topics[1].Bytes())
+
+	if len(vLog.Data) < 32 {
+		return nil
+	}
+
+	totalPot := new(big.Int).SetBytes(vLog.Data[0:32])
+
+	return &GameStartedEvent{
+		GameID:      gameID,
+		TotalPot:    totalPot,
+		BlockNumber: vLog.BlockNumber,
+		TxHash:      vLog.TxHash,
+	}
+}
+
+// parseGameEndedEvent parses a GameEnded event
+func (el *EventListener) parseGameEndedEvent(vLog types.Log) *GameEndedEvent {
+	if len(vLog.Topics) < 2 {
+		return nil
+	}
+
+	var gameID [32]byte
+	copy(gameID[:], vLog.Topics[1].Bytes())
+
+	// Parse arrays from data (complex parsing would use ABI)
+	// For simplicity, we'll return basic event
+	return &GameEndedEvent{
+		GameID:      gameID,
+		Winners:     []common.Address{},
+		Payouts:     []*big.Int{},
+		BlockNumber: vLog.BlockNumber,
+		TxHash:      vLog.TxHash,
+	}
+}
+
+// parseFundsLockedEvent parses a FundsLocked event
+func (el *EventListener) parseFundsLockedEvent(vLog types.Log) *FundsLockedEvent {
+	if len(vLog.Topics) < 2 {
+		return nil
+	}
+
+	var gameID [32]byte
+	copy(gameID[:], vLog.Topics[1].Bytes())
+
+	var player common.Address
+	copy(player[:], vLog.Topics[2].Bytes()[12:])
+
+	if len(vLog.Data) < 32 {
+		return nil
+	}
+
+	amount := new(big.Int).SetBytes(vLog.Data[0:32])
+
+	return &FundsLockedEvent{
+		GameID:      gameID,
+		Player:      player,
+		Amount:      amount,
+		BlockNumber: vLog.BlockNumber,
+		TxHash:      vLog.TxHash,
+	}
 }
 
 // WatchGameCreated watches for GameCreated events
 func (el *EventListener) WatchGameCreated(ctx context.Context, gameIDChan chan [32]byte) error {
-	// TODO: Implement using contract bindings
-	// This would use the generated contract's WatchGameCreated method
-	
-	logrus.Info("Watching for GameCreated events")
+	ch := make(chan interface{}, 10)
+	el.Subscribe("GameCreated", ch)
+
+	go func() {
+		for {
+			select {
+			case event := <-ch:
+				if gameCreated, ok := event.(*GameCreatedEvent); ok {
+					gameIDChan <- gameCreated.GameID
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
 	return nil
 }
 
 // WatchPlayerJoined watches for PlayerJoined events for a specific game
 func (el *EventListener) WatchPlayerJoined(ctx context.Context, gameID [32]byte, playerChan chan common.Address) error {
-	// TODO: Implement using contract bindings
-	
-	logrus.WithField("game_id", fmt.Sprintf("0x%x", gameID)).Info("Watching for PlayerJoined events")
+	ch := make(chan interface{}, 10)
+	el.Subscribe("PlayerJoined", ch)
+
+	go func() {
+		for {
+			select {
+			case event := <-ch:
+				if playerJoined, ok := event.(*PlayerJoinedEvent); ok {
+					if playerJoined.GameID == gameID {
+						playerChan <- playerJoined.Player
+					}
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
 	return nil
 }
 
@@ -210,8 +404,77 @@ func (el *EventListener) GetPastEvents(fromBlock, toBlock *big.Int) ([]types.Log
 
 // GetGameCreatedEvents retrieves all GameCreated events
 func (el *EventListener) GetGameCreatedEvents(fromBlock *big.Int) ([]GameCreatedEvent, error) {
-	// TODO: Implement using contract bindings
-	// This would parse logs and return typed events
-	
-	return []GameCreatedEvent{}, nil
+	logs, err := el.GetPastEvents(fromBlock, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	events := []GameCreatedEvent{}
+	gameCreatedSig := crypto.Keccak256Hash([]byte("GameCreated(bytes32,address,uint256,uint256)"))
+
+	for _, vLog := range logs {
+		if len(vLog.Topics) > 0 && vLog.Topics[0] == gameCreatedSig {
+			if event := el.parseGameCreatedEvent(vLog); event != nil {
+				events = append(events, *event)
+			}
+		}
+	}
+
+	return events, nil
+}
+
+// getPokerTableABI returns a simplified ABI for the PokerTable contract
+func getPokerTableABI() string {
+	return `[
+		{
+			"anonymous": false,
+			"inputs": [
+				{"indexed": true, "name": "gameId", "type": "bytes32"},
+				{"indexed": true, "name": "creator", "type": "address"},
+				{"indexed": false, "name": "buyIn", "type": "uint256"},
+				{"indexed": false, "name": "maxPlayers", "type": "uint256"}
+			],
+			"name": "GameCreated",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{"indexed": true, "name": "gameId", "type": "bytes32"},
+				{"indexed": true, "name": "player", "type": "address"},
+				{"indexed": false, "name": "amount", "type": "uint256"}
+			],
+			"name": "PlayerJoined",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{"indexed": true, "name": "gameId", "type": "bytes32"},
+				{"indexed": false, "name": "totalPot", "type": "uint256"}
+			],
+			"name": "GameStarted",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{"indexed": true, "name": "gameId", "type": "bytes32"},
+				{"indexed": false, "name": "winners", "type": "address[]"},
+				{"indexed": false, "name": "payouts", "type": "uint256[]"}
+			],
+			"name": "GameEnded",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{"indexed": true, "name": "gameId", "type": "bytes32"},
+				{"indexed": true, "name": "player", "type": "address"},
+				{"indexed": false, "name": "amount", "type": "uint256"}
+			],
+			"name": "FundsLocked",
+			"type": "event"
+		}
+	]`
 }
